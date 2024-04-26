c46f701ad2381fad94c81cc71f75b8b8b39969ee
haakim19 <haakimajmal7@gmail.com>
2024-04-26 09:26:14 +0530

    loop questions

---
+++ loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string greeting="hello world";
+    int count=1;
+
+    while (count<=5)
+    {
+        cout << greeting;
+    }
+    
+    return 0;
+
+}
