a204edc6d8b5353b99e6fcd3d4680b785ca80d27
haakim19 <haakimajmal7@gmail.com>
2024-04-28 13:26:15 +0530

    forloop

--- LEARN.py
+++ LEARN.py
@@ -1,4 +1,16 @@
+a=int(input("enter number a:"))
+b=int(input("enter number b:"))
+count=0
+even=[]
+odd=[]
+for i in range(a, b):
+    
+    if i%2==0:
+        even.append(i)
+        
+    else:
+        odd.append(i)
+print("Even numbers:",even)
+print("Odd numbers:",odd)
 
-name=['ahamed','haakim']
-for i in name:
-    print(i)
+    
